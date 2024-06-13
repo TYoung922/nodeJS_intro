a57f0e0d131cf7b601c22fac65f5d30f9172165e
TercanoNuruva <thyoung89@gmail.com>
2024-06-13 12:37:34 -0600

    update things

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const myController = require("../controllers");
 const routes = require("express").Router();
 
-routes.get("/home", myController.awesomeFunction);
+routes.get("/", myController.awesomeFunction);
 routes.get("/ttech", myController.myFunction);
 
 module.exports = routes;
