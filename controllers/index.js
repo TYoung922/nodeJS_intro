313b6ce5c40bb3760939a10161f7d710c67bb618
TercanoNuruva <thyoung89@gmail.com>
2024-06-13 12:00:33 -0600

    router

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const awesomeFunction = (req, res) => {
+  res.send("Hello World!");
+};
+
+const myFunction = (req, res) => {
+  res.send("Tooele Tech is Awesome!");
+};
+
+module.exports = { awesomeFunction, myFunction };
